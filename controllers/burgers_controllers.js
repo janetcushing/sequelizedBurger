1137c78f18b0c4e6563710d9995b3d76b4bcd285
janetcushing <janet.cushing@gmail.com>
2018-01-17 15:13:25 -0500

    deploying to heroku

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -24,22 +24,14 @@ function getBurgers(res) {
             console.log(err);
             res.status(500).end();
         } else if (data[0]) {
-            // console.log("about to dump burger_name");
-            // console.log("b name " + data[0].burger_name);
-            // console.log("devoured " + data[0].devoured);
             console.log("data is returned");
-            // console.log("custimer2: " + data[0].Customer.customer_name);
             console.log(data[0]);
             var uneaten = [];
             var eaten = [];
-            // console.log("data length " + data.length);
             for (let i = 0; i < data.length; i++) {
                 var burger = data[i].burger_name;
-                // console.log("boiger: " + burger);
                 var uneatenBurgerObject = [];
                 var eatenBurgerObject = [];
-                // console.log("ok then");
-                // console.log(JSON.stringify(data[i]));
                 if (data[i].devoured) {    
                     eatenBurgerObject = {
                         "customer_name": data[i].Customer.customer_name,
@@ -55,15 +47,7 @@ function getBurgers(res) {
                     }
                     uneaten.push(uneatenBurgerObject);
                 }
-                // console.log("uneatenBurgerObject: " + JSON.stringify(uneatenBurgerObject));
-                console.log("UNEATEN BURGER OBHECT LOADED");
-                // console.log("uneaten " + JSON.stringify(uneaten));
-
             }
-            // console.log("here i am");
-            // console.log("uneaten " + JSON.stringify(uneaten));
-            // console.log("eaten " + JSON.stringify(eaten));
-            // console.log("rendering");
             res.render("index", {
                 uneatenBurger: uneaten,
                 eatenBurger: eaten
@@ -80,7 +64,6 @@ function getBurgers(res) {
 }
 
 function updateBurgerTable(req, res) {
-    console.log("IM IN UDPATE BURGER TABLE");
     db.Burger.update({
         devoured: true,
     }, {
@@ -88,15 +71,13 @@ function updateBurgerTable(req, res) {
             id: req.params.id
         }
     }).then(function (data, err) {
-        console.log("data: ");
-        console.log(data);
-        console.log("err: ");
-        console.log(err);
         if (err) {
             // If an error occurred, send a generic server failure
+            console.log("an error occurred");
             console.log(err);
             return res.status(500).end();
         } else if (data.changedRows == 0) {
+            console.log("no rows returned");
             console.log(data);
             // If no rows were changed, then the ID must not exist, so 404
             console.log("burger row did not get updated");
@@ -109,34 +90,23 @@ function updateBurgerTable(req, res) {
 
 
 function createCustomerRow(req, res){
-    console.log("IM IN CREATE CUSTOMER ROW");
     db.Customer.create({
         customer_name: req.body.customer_name
     })
     .then(function (data, err) {
-        console.log(" i just inserted into customer");
-        console.log(data);
         if (err) {
             // If an error occurred, send a generic server failure
             console.log("AN error occured inserting into Customers");
             console.log(err);
             res.status(500).end();
         } else {
-            console.log("data ");
-            console.log(data);
-            console.log("Customer ");
-            console.log(data.id);
             var customer_id = data.id;
-            console.log("ZCUSTOMER_ID: " + customer_id);
             createBurgerRow(req, res, customer_id);
         }
     });
 }
 
 function createBurgerRow(req, res, customer_id){
-    console.log("IM IN CREATE BURGER ROW");
-    console.log("ZCUSTOMER_ID: " + customer_id);
-    console.log("burger_ID: " + req.body.burger_name);
     db.Burger.create({
         burger_name: req.body.burger_name,
         CustomerId: customer_id
@@ -148,7 +118,7 @@ function createBurgerRow(req, res, customer_id){
             console.log(err);
             res.status(500).end();
         } else {
-            console.log(data);
+            // console.log(data);
             getBurgers(res);;
         }
     });
